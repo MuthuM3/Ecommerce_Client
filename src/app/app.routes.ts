80a6fd6c3c545b683e0013c9285b33929a292c04
MuthuM3 <muthu.m3w@gmail.com>
2024-01-26 23:57:15 +0530

    route file

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,17 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
-  {path: 'signup', loadComponent: () => import('./signup/signup.component').then(m => m.SignupComponent)}
+  {path: '', redirectTo: 'home', pathMatch: "full"},
+  {
+    path: 'home',
+    loadComponent: () => import('./components/home/home.component').then((m) => m.HomeComponent),
+  },
+  {
+    path: 'signup',
+    loadComponent: () => import('./components/create-account/create-account.component').then((m) => m.SignupComponents),
+  },
+  {
+    path: 'signin',
+    loadComponent: () => import('./components/create-account/create-account.component').then((m) => m.SigninComponents),
+  },
 ];
